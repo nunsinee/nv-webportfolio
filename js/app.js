6e55d150f555617fb80413d5c863217cb624e8f4
Nunsinee Viken <71260513+nunsinee@users.noreply.github.com>
2022-01-24 20:13:34 +0100

    update contact form
    update contact form

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
 const contactForm = document.querySelector(".contact-form");
 
-let name = document.querySelector("#name");
-let email = document.querySelector("#email");
-let subject = document.querySelector("#subject");
-let message = document.querySelector("#message");
+const name = document.querySelector("#name");
+const contactNameError = document.querySelector("#contactNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const message = document.querySelector("#message");
+const textAreaError = document.querySelector("#textAreaError ");
 
 name.addEventListener("input", () => {
-	if (checkLength(name.value, 1) === true) {
+	if (checkLength(name.value.trim(), 0) === true) {
 		contactNameError.style.display = "none";
 	} else {
 		contactNameError.style.display = "block";
-	}
-});
-
-subject.addEventListener("input", () => {
-	if (checkLength(subject.value, 9) === true) {
-		subjectError.style.display = "none";
-	} else {
-		subjectError.style.display = "block";
 	}
 });
 
@@ -30,7 +24,7 @@ email.addEventListener("input", () => {
 });
 
 message.addEventListener("input", () => {
-	if (checkLength(message.value, 19) === true) {
+	if (checkLength(message.value.trim(), 19) === true) {
 		textAreaError.style.display = "none";
 	} else {
 		textAreaError.style.display = "block";
@@ -41,11 +35,9 @@ contactForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
 	if (
-		checkLength(name.value, 1) ||
-		(checkLength(name.value !== null) &&
-			checkLength(subject.value, 9) &&
-			validateEmail(email.value) &&
-			checkLength(message.value, 19))
+		checkLength(name.value, 1) &&
+		validateEmail(email.value) &&
+		checkLength(message.value, 19)
 	) {
 		senddingMail();
 		contactForm.reset();
@@ -77,26 +69,16 @@ function senddingMail() {
 	let formData = {
 		name: name.value,
 		email: email.value,
-		subject: subject.value,
 		message: message.value,
 	};
-	let xhr = new XMLHttpRequest();
-
-	xhr.open("POST", "/");
-	xhr.setRequestHeader("content-type", "application/json");
-	xhr.onload = function () {
-		console.log(xhr.responseText);
-		if (xhr.responseText == "success") {
-			//alert("email sent");
+	emailjs
+		.send("service_6noogkt", "template_xf6zs66", formData)
+		.then(function (res) {
+			console.log("success", res.status);
 			messageAlert.innerHTML = `<div class="alert alert-success"> Your message has been sent </div>`;
-			name.value = "";
-			email.value = "";
-			subject.value = "";
-			message.value = "";
-		} else {
-			//alert("something went wrong!");
+		})
+		.catch(function (err) {
+			console.log("not sent", err.status);
 			messageAlert.innerHTML = `<div class="alert alert-danger"> Sorry! something went wrong.</div>`;
-		}
-	};
-	xhr.send(JSON.stringify(formData));
+		});
 }
