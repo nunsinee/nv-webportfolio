7f26bf0e245f459c08b88fd04e38e2fb45837d09
Nunsinee Viken <71260513+nunsinee@users.noreply.github.com>
2022-01-24 14:00:24 +0100

    update server.js

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
 	console.log(req.body);
 	const transporter = nodemailer.createTransport({
-		service: "gmail",
+		host: "smtp.gmail.com",
+		port: 465,
+		secure: true,
 		auth: {
 			user: "nunsinee.v@gmail.com",
 			pass: "Mimmim2022",
